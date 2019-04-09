7a5dd6dba166f9aedb63d756a1b5f7689250bb65
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-09 14:38:26 -0400

    Adds custom content to the rendered <div> in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Take Me Out to the BALLLLLLLLLLLLLLLL Game!</h1>
+        <h1>Take Me Out to the BALLLLLLLLLLLLLLLLS Game!</h1>
+        <iframe
+          src="https://giphy.com/embed/l41lZ4JBFUi6iPciI"
+          width="480"
+          height="270"
+          frameBorder="0"
+          className="giphy-embed"
+          allowFullScreen
+        />
       </div>
     );
   }
