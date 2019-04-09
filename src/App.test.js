432545f1e86eebb8c72bf6b536ef013186fa3b6a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-09 17:02:12 -0400

    Creates a test suite for <App /> inside of App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -11,4 +11,17 @@ describe("<App />", () => {
 		ReactDOM.render(<App />, div);
 		ReactDOM.unmountComponentAtNode(div);
 	});
+
+	it('Take Me Out to the BALLLLLLLLLLS Game!', () => {
+		// const helpers = render(<App />); 
+		// the render method returns an object that gives you methods
+		// to help you find things in the DOM.
+
+		// console.log('helpers', helpers);
+	  
+		const { getByText, queryByText, debug } = render(<App />); 
+		
+		// getByText(/hello World/i);
+		expect(queryByText(/take me out to the balllllllllls game/i)).not.toBeNull();
+	  });
 });
