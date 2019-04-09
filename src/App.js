c6da487fef2cb87a8d757924e5d5ef20a530090d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-09 16:10:51 -0400

    Wires each <button> element to the proper event handler and to the proper function

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 import React, { Component } from "react";
 
-import Dashboard from "./components/dashboard/Dashboard";
 import Display from "./components/display/Display";
 import "./App.css";
 
 class App extends Component {
-	state = {
-		strikes: '0',
-		balls: '0'
-	};
-
-	handleStrikesChange = () => {
-		console.log("The handleClick() function has been activated!");
-	}
-
-	handleBallsChange = () => {
-		console.log("The handleClick() function has been activated!");
-	}
 
   	render() {
 		return (
 		<div className="App">
 			<h1 className="App-title">Take Me Out to the BALLLLLLLLLLLLLLLLS Game!</h1>
-			<Display 
-				strikes={this.state.strikes}
-				balls={this.state.balls} 
-			/>
-			<Dashboard
-				funcStrike={this.strike}
-				funcBall={this.ball} 
-				funcFoul={this.foul} 
-				funcHit={this.hit} 
-			/>
+			<Display />
 		</div>
 		);
-	};
-	  
-	strike = () => {
-		console.log("The strike() function was triggered!");
-		this.handleStrikesChange();
-		this.handleBallsChange();
-		// this.setState({});
-	};
-	
-	ball = () => {
-		console.log("The ball() function was triggered!");
-		this.handleStrikesChange();
-		this.handleBallsChange();
-		// this.setState({});
-	};
-
-	foul = () => {
-		console.log("The foul() function was triggered!");
-		this.handleStrikesChange();
-		this.handleBallsChange();
-		// this.setState({});
-	};
-	
-	hit = () => {
-		console.log("The hit() function was triggered!");
-		this.handleStrikesChange();
-		this.handleBallsChange();
-		// this.setState({});
 	};
 }
 
--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,25 +1,83 @@
 import React from 'react';
 
+import Dashboard from "../dashboard/Dashboard";
 import "../../App.css";
 
-function Display(props) {
-    return (
-        <div className="Display">
-            {/* eslint-disable-next-line*/}
-            <iframe
-                src="https://giphy.com/embed/l41lZ4JBFUi6iPciI"
-                width="480"
-                height="270"
-                frameBorder="0"
-                className="giphy-embed"
-                allowFullScreen
-            />
-            <div className="scoreboard">
-                <div className="strikes">Strikes: {props.strikes}</div>
-                <div className="balls">Balls: {props.balls}</div>
+class Display extends React.Component {
+	state = {
+		strikes: 0,
+		balls: 0
+	};
+
+	handleStrikesChange = () => {
+		console.log("The handleClick() function has been activated!");
+
+		this.setState((prevState, { strikes }) => ({
+			strikes: prevState.strikes + 1,
+		}));
+	}
+
+	handleBallsChange = () => {
+		console.log("The handleClick() function has been activated!");
+
+		this.setState((prevState, { balls }) => ({
+			balls: prevState.balls + 1,
+		}));
+	}
+
+    render() {
+        return (
+            <div className="Display">
+                {/* eslint-disable-next-line*/}
+                <iframe
+                    src="https://giphy.com/embed/l41lZ4JBFUi6iPciI"
+                    width="480"
+                    height="270"
+                    frameBorder="0"
+                    className="giphy-embed"
+                    allowFullScreen
+                />
+                <div className="scoreboard">
+                    <div className="strikes">Strikes: {this.state.strikes}</div>
+                    <div className="balls">Balls: {this.state.balls}</div>
+                </div>
+                <Dashboard 
+                    funcStrike={this.strike}
+                    funcBall={this.ball}
+                    funcFoul={this.foul}
+                    funcHit={this.hit}
+                />
             </div>
-        </div>
-    )
+        );
+    };
+
+	strike = () => {
+		console.log("The strike() function was triggered!");
+
+		this.handleStrikesChange();
+		this.handleBallsChange();
+	};
+	
+	ball = () => {
+		console.log("The ball() function was triggered!");
+
+		this.handleStrikesChange();
+		this.handleBallsChange();
+	};
+
+	foul = () => {
+		console.log("The foul() function was triggered!");
+
+		this.handleStrikesChange();
+		this.handleBallsChange();
+	};
+	
+	hit = () => {
+		console.log("The hit() function was triggered!");
+
+		this.handleStrikesChange();
+		this.handleBallsChange();
+	};
 }
 
 export default Display;
