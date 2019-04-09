aa1582ab29e9a9a484310b7e0af2bd9e416f106a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-09 16:29:49 -0400

    Creates a 'describe()' statement in App.test.js to hold test cases for <App />

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render, fireEvent } from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
 
-import App from './App';
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App />", () => {
+	it("renders without crashing", () => {
+		const div = document.createElement("div");
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
 });
