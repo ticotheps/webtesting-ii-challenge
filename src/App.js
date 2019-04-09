48de20a7083ee69636a33d182639ea1afc251e46
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-09 16:27:40 -0400

    Imports '{ render, fireEvent }' from 'react-testing-library', as well as 'jest-dom/extend-expect', into App.test.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   	render() {
 		return (
 		<div className="App">
-			<h1 className="App-title">Take Me Out to the BALLLLLLLLLLLLLLLLS Game!</h1>
+			<h1 className="App-title">Take Me Out to the BALLLLLLLLLLS Game!</h1>
 			<Display />
 		</div>
 		);
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
 import App from './App';
 
 it('renders without crashing', () => {
